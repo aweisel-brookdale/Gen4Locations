e8d4c401a6071b635d38ece9187bfd01ec7fa44d
aweisel-brookdale <aweisel1@my.brookdalecc.edu>
2024-12-05 13:51:38 -0500

    Fixed CSS and links

--- client/js/write-data.js
+++ client/js/write-data.js
@@ -1,8 +1,39 @@
 //Create a listener that waits for user to enter submit button
-//Submit Library books
+//Submit dex entries
 $('#data-submit').click(function() {
-    alert("Submit Button Pushed");
+    //alert("Submit Button Pushed");
+    var dexNumber = $('#dexNumber').val();
+    var name = $('#name').val();
+    var type = $('#type').val();
+    var availability = $('#availability').val();
+    var obtain = $('#obtain').val();
 
+    var jsonString = {
+        dexNumber:dexNumber, 
+        name:name, 
+        type:type, 
+        availability:availability, 
+        obtain:obtain
+    }
+
+    $.ajax({
+        url: dexURL + "/write-record",
+        type: "post",
+        data: jsonString,
+        success: function(response) {
+            var data = JSON.parse(response);
+            if(data.msg = "SUCCESS") {
+                alert("Data Saved");
+            } else {
+                console.log(data.msg);
+            }
+        },
+        error: function(err) {
+            console.log(err);
+        }
+    });
+
+ });
 
 });
 
